07cf969b5c2a30bcd8b0c3d609b1ae0cb746077c
Matthew Young <luckymatty@gmail.com>
2017-11-05 16:39:55 -0800

    Implement map feature

---
+++ GitMe/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitMe
+//
+//  Created by Matthew Young on 11/5/17.
+//  Copyright © 2017 Matthew Young. All rights reserved.
+//
+
+import UIKit
+
+class MapFrItinerary: NSObject {
+    
+}
